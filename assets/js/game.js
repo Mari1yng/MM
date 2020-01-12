b326a1a34f6f4474845ce4009d334ac426eedfb7
MalC-U <mwcalow@gmail.com>
2020-01-12 18:30:56 +0000

    Uploaded all necessary  images and created allCards array storing them

---
+++ assets/js/game.js
@@ -0,0 +1,1 @@
+let allCards = ['mario.png', 'mario.png','luigi.png', 'luigi.png', 'leonardo.png', 'leonardo.png', 'robin.png', 'robin.png', 'frogger.png', 'frogger.png', 'dnkeykong.png', 'donkeykong.png', 'ghost.png', 'ghost.png', 'bombjack.png', 'bombjack.png'];
