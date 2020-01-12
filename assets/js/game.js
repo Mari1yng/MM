832e898716325270b6ef460fd046e54ebc26d2ce
MalC-U <mwcalow@gmail.com>
2020-01-12 21:10:35 +0000

    Created function checkButton and added event listener

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,10 @@
-let allCards = ['mario.png', 'mario.png','luigi.png', 'luigi.png', 'leonardo.png', 'leonardo.png', 'robin.png', 'robin.png', 'frogger.png', 'frogger.png', 'dnkeykong.png', 'donkeykong.png', 'ghost.png', 'ghost.png', 'bombjack.png', 'bombjack.png'];
+let allCards = ['mario.png', 'mario.png','luigi.png', 'luigi.png', 'leonardo.png', 'leonardo.png', 'robin.png', 'robin.png', 'frogger.png', 'frogger.png', 'donkeykong.png', 'donkeykong.png', 'ghost.png', 'ghost.png', 'bombjack.png', 'bombjack.png'];
+let buttons = [...document.getElementsByTagName('button')];
+
 let visibleTiles = 0;
+let activeButton;
+let deck;
+
 // Shuffle all cards using Fisher-Yates shuffle algorithm
 Array.prototype.allCardsShuffle = function() {
     let newPosition;
@@ -16,4 +21,10 @@ Array.prototype.allCardsShuffle = function() {
 
 
 
+let checkButton = function (){
+    buttons.forEach(function(button){
+        button.addEventListener('click', clickedButton);
+    });
+}
 
+checkButton();
