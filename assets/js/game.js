fb77720cd5830218a7dce2652f8d5b2b586a935f
MalC-U <mwcalow@gmail.com>
2020-01-13 14:51:10 +0000

    corrected tag class setup in order to display it properly

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 let allCards = ['mario.png', 'mario.png','luigi.png', 'luigi.png', 'leonardo.png', 'leonardo.png', 'robin.png', 'robin.png', 'frogger.png', 'frogger.png', 'donkeykong.png', 'donkeykong.png', 'ghost.png', 'ghost.png', 'bombjack.png', 'bombjack.png'];
 let buttons = [...document.getElementsByTagName('button')];
-
+let tiles =[...document.getElementsByClassName('tile')];
 
 let visibleTiles = 0;
 let activeButton;
@@ -25,7 +25,7 @@ function createGrid (){
     let newGrid = '';
 //making sure the grid is created from the available deck - after checkButton () ran
     for (i = 0; i < deck.length; i++){
-    newGrid += '<div id="tile' + i + ' class="tile" onclick="startGame(this,\'' + deck[i] + '\')"></div>';
+    newGrid += '<div id="tile' + i + '" onclick="startGame(this,\'' + deck[i] + '\')"class="tile"></div>';
     }; 
     document.getElementById('board').innerHTML = newGrid;
 }
@@ -35,17 +35,14 @@ let clickedButton = function(){
     activeButton = this;
     if ($(this).hasClass('easybtn')){
         deck = deck = ['mario.png', 'mario.png','luigi.png', 'luigi.png', 'leonardo.png', 'leonardo.png', 'robin.png', 'robin.png'];
-        
         deck.allCardsShuffle();
         createGrid ();
     } else if ($(this).hasClass('mediumbtn')){
         deck =  ['mario.png', 'mario.png','luigi.png', 'luigi.png', 'leonardo.png', 'leonardo.png', 'robin.png', 'robin.png', 'frogger.png', 'frogger.png', 'donkeykong.png', 'donkeykong.png'];
-        
         deck.allCardsShuffle();
         createGrid ();
     } else {
         deck = allCards;
-        
         deck.allCardsShuffle();
         createGrid ();
     }
@@ -60,7 +57,6 @@ let checkButton = function (){
 
 checkButton();  
 
-//Creating startGame function that will allow user to start uncovering what's under each tile
 function startGame(){
 
 }
